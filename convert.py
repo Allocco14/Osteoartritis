fcf43f4ba485bdbd806aa6b0386c5d5129d5df03
Álvaro Chaves Ladino <achavesl@unal.edu.co>
2024-03-09 19:50:00 -0500

    update gitignore

--- convert.py
+++ convert.py
@@ -3,41 +3,46 @@ from PIL import Image
 import os
 from tqdm import tqdm
 import time
+from multiprocessing import Pool, cpu_count
+
+def convert_tile(tile_info):
+    input_path, output_path, level, tile_size, i, j = tile_info
+    slide = open_slide(input_path)
+    region = slide.read_region(
+        (i * (2 ** level), j * (2 ** level)), level, (tile_size, tile_size))
+    region = region.convert("RGB")
+    output_file = os.path.join(output_path, f"tile_{i}_{j}_level_{level}.png")
+    region.save(output_file, "PNG")
 
 def convert_svs_to_png(input_path, output_path, tile_size, level):
-    # Abre la imagen SVS
-    slide = open_slide(input_path)
-    
-    # Crea el directorio de salida si no existe
+    # Create the output directory if it doesn't exist
     os.makedirs(output_path, exist_ok=True)
-    
-    # Obtiene dimensiones de la imagen en el nivel especificado
+    # Get dimensions of the image at the specified level
+    slide = open_slide(input_path)
     width, height = slide.level_dimensions[level]
-    
     tiles_x = width // tile_size + 1
     tiles_y = height // tile_size + 1
-
     total = tiles_x * tiles_y
-
     start_time = time.time()
-
-    # Itera sobre las tiles y las guarda como PNG
-    with tqdm(total=total) as pbar:
-        for i in range(0, width, tile_size):
-            for j in range(0, height, tile_size):
+    tile_info_list = []
+    for i in range(0, width, tile_size):
+        for j in range(0, height, tile_size):
+            tile_info_list.append((input_path, output_path, level, tile_size, i, j))
+    # Multiprocessing
+    with Pool(cpu_count()) as p:
+        print(p, cpu_count())
+        with tqdm(total=total) as pbar:
+            for _ in p.imap_unordered(convert_tile, tile_info_list):
                 pbar.update(1)
-                region = slide.read_region((i * (2 ** level), j * (2 ** level)), level, (tile_size, tile_size))
-                region = region.convert("RGB")
-                output_file = os.path.join(output_path, f"tile_{i}_{j}_level_{level}.png")
-                region.save(output_file, "PNG")
-
     end_time = time.time()
-    print(f"Tiempo total: {end_time - start_time} segundos")
+    print(f"Total time: {end_time - start_time} seconds")
 
 if __name__ == "__main__":
-    tile_size = 1024  # Puedes ajustar este valor según tus necesidades
-    target_level = 0  # Nivel al que deseas acceder, ajusta según tus necesidades
-    input_image_path = r"/app/data/643-1-19.svs"  # Usa la 'r' para indicar una cadena cruda en Windows
+    tile_size = 1024  # You can adjust this value according to your needs
+    target_level = 0  # Level you want to access, adjust as needed
+    # Use 'r' to indicate a raw string in Windows
+    filename = "202-19"
+    input_image_path = r"/app/data/" + filename + ".svs"
     output_directory = f"/app/data/output/{target_level}_{tile_size}/"
-
-    convert_svs_to_png(input_image_path, output_directory, tile_size, target_level)
+    convert_svs_to_png(input_image_path, output_directory,
+                       tile_size, target_level)
